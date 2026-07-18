25bd972172ebf9b8f105f0d3d881e1c8fc8a5f12
Manasa V <manuvenke06@gmail.com>
2026-07-18 19:50:29 +0530

    Allow backend-served contest image URLs

--- next.config.ts
+++ next.config.ts
@@ -16,9 +16,21 @@ const nextConfig: NextConfig = {
             },
             {
                 protocol: "http",
+                hostname: "10.10.10.23",
+                port: "8000",
+                pathname: "/api/v1/images/**",
+            },
+            {
+                protocol: "http",
                 hostname: "localhost",
                 port: "9000",
                 pathname: "/**",
+            },
+            {
+                protocol: "http",
+                hostname: "localhost",
+                port: "8000",
+                pathname: "/api/v1/images/**",
             },
         ],
     },
